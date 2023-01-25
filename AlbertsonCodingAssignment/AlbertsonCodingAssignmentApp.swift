95478bc2e0fb366b421b06be024c1030894fa37b
sammy2997 <samueladama@ymail.com>
2023-01-25 12:06:43 -0600

    Initial Commit

---
+++ AlbertsonCodingAssignment/AlbertsonCodingAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertsonCodingAssignmentApp.swift
+//  AlbertsonCodingAssignment
+//
+//  Created by Samuel Adama on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AlbertsonCodingAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
